22ef80b5d208628c03e5a404bf29ecf1903b4d8e
Adiks <apacholak97@interia.pl>
2022-06-17 10:05:26 +0200

    using MapStruct

---
+++ src/test/java/guru/springfamework/spring5mvcrest/mapper/CategoryMapperTest.java
@@ -0,0 +1,31 @@
+package guru.springfamework.spring5mvcrest.mapper;
+
+import guru.springfamework.api.v1.mapper.CategoryMapper;
+import guru.springfamework.api.v1.model.CategoryDTO;
+import guru.springfamework.domain.Category;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class CategoryMapperTest {
+
+    public static final long ID = 1L;
+    public static final String CATEGORY = "category";
+
+    CategoryMapper categoryMapper = CategoryMapper.INSTANCE;
+
+    @Test
+    public void categoryToCategoryDTO() {
+
+        Category category = new Category();
+        category.setId(ID);
+        category.setName(CATEGORY);
+
+        CategoryDTO categoryDTO = categoryMapper.categoryToCategoryDTO(category);
+
+        assertNotNull(categoryDTO);
+        assertEquals(Long.valueOf(ID), categoryDTO.getId());
+        assertEquals(CATEGORY, categoryDTO.getName());
+    }
+}
